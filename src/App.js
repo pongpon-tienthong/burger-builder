4a1a502cfab0d383cf77cb8227979663da52d5d2
Pongpon Tienthong <pongpon.tienthong@gmail.com>
2018-05-10 10:50:58 -0500

    create BugerBuilder component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import Layout from './components/Layout/Layout';
+import BurgerBuilder from './components/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Layout>
-          <p>Thanks God. It's working.</p>
+          <BurgerBuilder />
         </Layout>
       </div>
     );
