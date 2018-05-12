3ae0330fccff78eda14a41de99d53b3bcb7c15f0
Pongpon Tienthong <pongpon.tienthong@gmail.com>
2018-05-12 16:39:12 -0500

    add 'Order Now!' btn

--- src/components/BurgerBuilder/BurgerBuilder.js
+++ src/components/BurgerBuilder/BurgerBuilder.js
@@ -19,7 +19,17 @@ class BurgerBuilder extends Component {
       cheese: 0,
       meat: 0
     },
-    totalPrice: 4
+    totalPrice: 4,
+    purchasable: false
+  }
+
+  updatePurchaseState = (ingredients) => {
+
+    const sum = Object.keys(ingredients)
+      .map(igKey => ingredients[igKey])
+      .reduce((sum, curr) => sum + curr, 0);
+
+    this.setState({ purchasable: sum > 0 });
   }
 
   addIngredientHandler = (type) => {
@@ -36,6 +46,8 @@ class BurgerBuilder extends Component {
       totalPrice: newPrice,
       ingredients: updatedIngredients
     });
+
+    this.updatePurchaseState(updatedIngredients);
   }
 
   removeIngredientHandler = (type) => {
@@ -56,6 +68,8 @@ class BurgerBuilder extends Component {
       totalPrice: newPrice,
       ingredients: updatedIngredients
     });
+
+    this.updatePurchaseState(updatedIngredients);
   }
 
   render() {
@@ -75,6 +89,7 @@ class BurgerBuilder extends Component {
           ingredientAdded={this.addIngredientHandler}
           ingredientRemoved={this.removeIngredientHandler}
           disabled={disabledButtons}
+          purchasable={this.state.purchasable}
           price={this.state.totalPrice} />
       </Aux>
     );
