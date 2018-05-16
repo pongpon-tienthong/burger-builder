73f54d330af33324bc04b653b16ec91a1abd2909
Pongpon Tienthong <pongpon.tienthong@gmail.com>
2018-05-16 10:54:39 -0500

    add spinner

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,16 +1,18 @@
-import React, { Component } from 'react';
-import classes from './Modal.css';
-import Aux from '../../../hoc/Aux/Aux';
-import Backdrop from '../Backdrop/Backdrop';
+import React, { Component } from "react";
+import classes from "./Modal.css";
+import Aux from "../../../hoc/Aux/Aux";
+import Backdrop from "../Backdrop/Backdrop";
 
 class Modal extends Component {
-
   shouldComponentUpdate(nextProps, nextState) {
-    return nextProps.show !== this.props.show;
+    return (
+      nextProps.show !== this.props.show ||
+      nextProps.children !== this.props.children
+    );
   }
 
   componentWillUpdate() {
-    console.log('[Model] componentWillUpdate');
+    console.log("[Model] componentWillUpdate");
   }
 
   render() {
@@ -20,9 +22,10 @@ class Modal extends Component {
         <div
           className={classes.Modal}
           style={{
-            transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
-            opacity: this.props.show ? '1' : '0'
-          }}>
+            transform: this.props.show ? "translateY(0)" : "translateY(-100vh)",
+            opacity: this.props.show ? "1" : "0"
+          }}
+        >
           {this.props.children}
         </div>
       </Aux>
