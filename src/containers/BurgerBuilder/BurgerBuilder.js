776647eb7745b0257c7eb34a448e63aeb6952959
Pongpon Tienthong <pongpon.tienthong@gmail.com>
2018-05-16 11:18:04 -0500

    global error handlering

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -6,6 +6,7 @@ import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
 import axios from "../../axios-orders";
 import Spinner from "../../components/UI/Spinner/Spinner";
+import withErrorHandler from "../../hoc/withErrorHandler/withErrorHandler";
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -98,7 +99,7 @@ class BurgerBuilder extends Component {
     };
 
     axios
-      .post("/orders.json", order)
+      .post("/orders", order)
       .then(res => {
         this.setState({ loading: false, purchasing: false });
         console.log(res);
@@ -153,4 +154,4 @@ class BurgerBuilder extends Component {
   }
 }
 
-export default BurgerBuilder;
+export default withErrorHandler(BurgerBuilder, axios);
